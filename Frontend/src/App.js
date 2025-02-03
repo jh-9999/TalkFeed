71fe650e3946b929a3e656b2bde1641ce798e45a
이정현 <wjd1gus2@gmail.com>
2025-02-03 11:00:19 +0900

    1
    1

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,18 +1,11 @@
-import Main from "./components/Main"
+import Main from './components/Main';
 
 const App = () => {
-  const username = ["kim","lee"]
-
-  const onclick = () =>{
-    alert("안녕하세요")
-  } 
-
   return (
     <div>
-      <Main user={username[2]} handleclick={onclick} />
+      <Main />
     </div>
-    
-  )
+  );
 };
 
 export default App;
